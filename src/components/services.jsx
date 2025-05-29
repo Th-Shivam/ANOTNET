dffb73871cf97a30a50f31c7a1cbaa8805e1ac26
Th-Shivam <dreamyshivam01@gmail.com>
2025-05-29 09:38:59 +0530

    updated services

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -93,32 +93,32 @@ export function OurServices() {
           <GridItem
             area="md:[grid-area:1/1/2/7] xl:[grid-area:1/1/2/5]"
             icon={<Box className="h-4 w-4 text-white dark:text-neutral-400" />}
-            title="Do things the right way"
-            description="Running out of copy so I'll write anything."
+            title="Coming Soon"
+            description="Coming Soon"
           />
           <GridItem
             area="md:[grid-area:1/7/2/13] xl:[grid-area:2/1/3/5]"
             icon={<Settings className="h-4 w-4 text-white dark:text-neutral-400" />}
-            title="The best AI code editor ever."
-            description="Yes, it's true. I'm not even kidding. Ask my mom if you don't believe me."
+            title="Coming Soon"
+            description="Coming Soon"
           />
           <GridItem
             area="md:[grid-area:2/1/3/7] xl:[grid-area:1/5/3/8]"
             icon={<Lock className="h-4 w-4 text-white dark:text-neutral-400" />}
-            title="You should buy Aceternity UI Pro"
-            description="It's the best money you'll ever spend"
+            title="Coming Soon"
+            description="Coming Soon"
           />
           <GridItem
             area="md:[grid-area:2/7/3/13] xl:[grid-area:1/8/2/13]"
             icon={<Sparkles className="h-4 w-4 text-white dark:text-neutral-400" />}
-            title="This card is also built by Cursor"
-            description="I'm not even kidding. Ask my mom if you don't believe me."
+            title="Coming Soon"
+            description="Coming Soon"
           />
           <GridItem
             area="md:[grid-area:3/1/4/13] xl:[grid-area:2/8/3/13]"
             icon={<Search className="h-4 w-4 text-white dark:text-neutral-400" />}
-            title="Coming soon on Aceternity UI"
-            description="I'm writing the code as I record this, no shit."
+            title="Coming Soon"
+            description="Coming Soon"
           />
         </ul>
       </div>
